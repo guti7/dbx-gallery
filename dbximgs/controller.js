dd1f97b5727ba73dc0bc8bac8c357fd1a123b864
guti7 <7505855+guti7@users.noreply.github.com>
2018-03-25 22:59:05 -0700

    Fix missing semicolon

--- dbximgs/controller.js
+++ dbximgs/controller.js
@@ -2,4 +2,4 @@ module.exports.home = (req, res, next) => {
   var paths = ['images/sample-1.jpg', 'images/sample-2.jpg', 'images/sample-3.jgp'];
   
   res.render('gallery', { imgs: paths, layout: false });
-}
+};
