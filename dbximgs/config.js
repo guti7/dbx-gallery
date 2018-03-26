53c3500d2dc6a1c29906cbf7ca809748d474e932
guti7 <7505855+guti7@users.noreply.github.com>
2018-03-26 08:27:17 -0700

    Add configuration items

---
+++ dbximgs/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  DBX_API_DOMAIN:     'https://api.dropboxapi.com',
+  DBX_OAUTH_DOMAIN:   'https://www.dropbox.com',
+  DBX_OAUTH_PATH:     '/oauth2/authorize',
+  DBX_TOKEN_PATH:     '/oauth2/token',
+  DBX_APP_KEY:        '<get_appkey_from_dbx>',
+  DBX_APP_SECRET:     '<get_appsecrect_from_dbx>',
+  OAUTH_REDIRECT_URL: 'http://localhost:3000/oauthredirect',
+}
