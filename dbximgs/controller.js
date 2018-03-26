bc579f47daf4833d288c962ddbcab2296dda1b58
guti7 <7505855+guti7@users.noreply.github.com>
2018-03-25 23:10:35 -0700

    Fix display of third image

--- dbximgs/controller.js
+++ dbximgs/controller.js
@@ -1,5 +1,5 @@
 module.exports.home = (req, res, next) => {
-  var paths = ['images/sample-1.jpg', 'images/sample-2.jpg', 'images/sample-3.jgp'];
+  var paths = ['images/sample-1.jpg', 'images/sample-2.jpg', 'images/sample-3.jpg'];
   
   res.render('gallery', { imgs: paths, layout: false });
 };
