022b81bdce17c55214d640eaceaf5fe628ce8a4d
guti7 <7505855+guti7@users.noreply.github.com>
2018-03-26 10:12:06 -0700

    Configure environment

--- dbximgs/config.js
+++ dbximgs/config.js
@@ -1,9 +1,11 @@
+require('dotenv').config(silent: true});
+
 module.exports = {
   DBX_API_DOMAIN:     'https://api.dropboxapi.com',
   DBX_OAUTH_DOMAIN:   'https://www.dropbox.com',
   DBX_OAUTH_PATH:     '/oauth2/authorize',
   DBX_TOKEN_PATH:     '/oauth2/token',
-  DBX_APP_KEY:        '<get_appkey_from_dbx>',
-  DBX_APP_SECRET:     '<get_appsecrect_from_dbx>',
-  OAUTH_REDIRECT_URL: 'http://localhost:3000/oauthredirect',
+  DBX_APP_KEY:        process.env.DBX_APP_KEY,
+  DBX_APP_SECRET:     process.env.DBX_APP_SECRET,
+  OAUTH_REDIRECT_URL: process.env.OAUTH_REDIRECT_URL,
 }
